5ec2e14c061cd8191f43eee1fb6b5231faf7b858
ivanfslee <ivanfslee@gmail.com>
2019-12-31 20:09:29 -0800

    Added another form field and reused useInputState hook in SimpleFormInputHook component

--- src/SimpleFormInputHook.js
+++ src/SimpleFormInputHook.js
@@ -3,11 +3,14 @@ import useInputState from './hooks/useInputState';
 
 export default function SimpleFormInputHook() {
     const [email, updateEmail, resetEmail] = useInputState('');
+    const [password, updatePassword, resetPassword] = useInputState('');
     return (
         <div>
-            <h1>The value is...{email}</h1>
+            <h1>Email: {email}  &  Password: {password}</h1>
             <input type="text" value={email} onChange={updateEmail}/>
+            <input type="text" value={password} onChange={updatePassword}/>
             <button onClick={resetEmail}>Clear Email</button>
+            <button onClick={resetPassword}>Clear Password</button>
         </div>
     );
 }
