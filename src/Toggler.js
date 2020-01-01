312895f8fb4e68dbf2627218eb0dea1c43eafad2
ivanfslee <ivanfslee@gmail.com>
2019-12-31 19:44:33 -0800

    Added SimpleFormClass and SimpleFormHooks. Both have same functionality, except one uses class based component. The other uses hooks.

--- src/Toggler.js
+++ src/Toggler.js
@@ -1,14 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 import useToggle from './hooks/useToggle';
 
 function Toggler() {
     const [isHappy, toggleIsHappy] = useToggle(true);
     const [isHeartBroken, toggleIsHeartBroken] = useToggle(false);
-
+    const [isGlasses, toggleIsGlasses] = useToggle(true);
     return (
         <div>
             <h1 onClick={toggleIsHappy}>{isHappy ? ':)' : ':('}</h1>
             <h1 onClick={toggleIsHeartBroken}>{isHeartBroken ? '</3' : '<3'}</h1>
+            <h1 onClick={toggleIsGlasses}>{isGlasses ? 'B-)' : ':-)'}</h1>
         </div>
         
     );
